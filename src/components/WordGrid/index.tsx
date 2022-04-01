73b0579d2d63dca2ed3e813dc8fccf93b52c66fc
Oskari Rautiainen <orautiainen@splunk.com>
2022-04-01 08:25:03 -0700

    Working word validation

--- src/components/WordGrid/index.tsx
+++ src/components/WordGrid/index.tsx
@@ -8,7 +8,7 @@ interface Props {
   numTries?: number;
 }
 
-interface Words extends Array<Array<string>> {}
+interface Words extends Array<string> {}
 export enum Result {
   NoMatch = 0,
   WrongPlacement = 1,
@@ -16,37 +16,89 @@ export enum Result {
 }
 interface Results extends Array<Array<Result>> {}
 
+const CorrectWord = "sound";
+
+const validate = (word: string, correctWord: string) => {
+  const results = [];
+  for (let i = 0; i < word.length; i++) {
+    const char = word.charAt(i);
+    const indexInCorrect = correctWord.indexOf(char);
+    if (indexInCorrect < 0) {
+      results.push(Result.NoMatch);
+    } else if (indexInCorrect >= 0) {
+      if (indexInCorrect === i) {
+        results.push(Result.Correct);
+      } else {
+        results.push(Result.WrongPlacement);
+      }
+    }
+  }
+  return results;
+};
+
 export const WordGrid = ({ wordLength = 5, numTries = 6 }: Props) => {
-  const [words, setWords] = useState<Words>(Array(numTries));
+  const [words, setWords] = useState<Words>(Array(numTries).fill(""));
   const [results, setResults] = useState<Results>(Array(numTries));
   const [currentRow, setCurrentRow] = useState(0);
 
-  useEffect(() => {
-    const newWords = Array(numTries);
-    for (let i = 0; i < numTries; i++) {
-      newWords[i] = Array(wordLength).fill("");
+  const handleKeyDown = (e: any) => {
+    console.log({ key: e.key, word: words[currentRow], currentRow, words });
+    if (e.keyCode === 8) {
+      // Delete key
+      if (words[currentRow].length > 0) {
+        setWords((oldWords) => {
+          const newWords = [...oldWords];
+          const curWord = newWords[currentRow];
+          newWords[currentRow] = curWord.substring(0, curWord.length - 1);
+          return newWords;
+        });
+      }
+    } else if (e.keyCode === 13) {
+      // Enter key
+      if (
+        words[currentRow].length === wordLength &&
+        currentRow < numTries - 1
+      ) {
+        const newResults = validate(words[currentRow], CorrectWord);
+        setResults((oldResults) => {
+          oldResults[currentRow] = newResults;
+          return oldResults;
+        });
+        setCurrentRow(currentRow + 1);
+      }
+    } else if (e.key.toLowerCase() >= "a" && e.key <= "z") {
+      // alpha numeric
+      if (words[currentRow].length < wordLength) {
+        setWords((oldWords) => {
+          const newWords = [...oldWords];
+          newWords[currentRow] += e.key.toLowerCase();
+          console.log({ newWords });
+          return newWords;
+        });
+      }
     }
+  };
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyDown);
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  });
+
+  useEffect(() => {
+    const newWords = Array(numTries).fill("");
     setWords(newWords);
   }, [wordLength, numTries]);
   //@ts-ignore
-  const createChangeHandler = (w) => (e, l) => {
-    const val = e.target.value.substring(0, 1);
-    console.log({ eval: e.target.value, val });
-    const newW = [...words];
-    newW[w][l] = val;
-    setWords(newW);
-  };
-  console.log({ words });
+  console.log({ words, results });
   return (
     <Container maxWidth="sm">
       <Stack>
         {words.map((w, i) => (
           <WordRow
             key={i}
-            letters={w}
-            active={currentRow === i}
-            //@ts-ignore
-            onChange={createChangeHandler(i)}
+            word={w}
+            wordLength={wordLength}
             result={results[i]}
           />
         ))}
--- src/components/WordRow/index.tsx
+++ src/components/WordRow/index.tsx
@@ -2,28 +2,43 @@ import React from "react";
 import Box from "@mui/material/Box";
 import Stack from "@mui/material/Stack";
 import TextField from "@mui/material/TextField";
+import Paper from "@mui/material/Paper";
 import { Result } from "../WordGrid";
 
 interface Props {
-  letters: Array<string>;
-  active: boolean;
+  word: string;
   result: Array<Result>;
-  onChange: () => {};
+  wordLength: number;
 }
 
-export const WordRow = ({ letters, active, result, onChange }: Props) => {
+// Incorrect, Wrong Placement, Correct
+const bgColors = ["none", "orange", "green"];
+
+export const WordRow = ({ word, wordLength, result }: Props) => {
+  const letters = [
+    ...word.split(""),
+    ...Array(wordLength - word.length).fill(""),
+  ];
   return (
     <Stack direction="row">
       {letters.map((letter, i) => (
-        <TextField
+        <Paper
           key={i}
-          disabled={!active}
-          id="outlined-basic"
           variant="outlined"
-          // @ts-ignore
-          onChange={(e) => onChange(e, i)}
-          value={letter}
-        />
+          square
+          sx={{
+            width: 100,
+            height: 100,
+            lineHeight: "100px",
+            textAlign: "center",
+            fontSize: "xxx-large",
+            textTransform: "uppercase",
+            backgroundColor:
+              result && result[i] in bgColors ? bgColors[result[i]] : "none",
+          }}
+        >
+          <span>{letter}</span>
+        </Paper>
       ))}
     </Stack>
   );
